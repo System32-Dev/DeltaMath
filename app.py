d12c3f3455bc6888270202ac93961ab9627fd705
System32-Dev <83769295+System32-Dev@users.noreply.github.com>
2024-04-03 21:55:56 -0400

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ class MyServer(BaseHTTPRequestHandler):
         self.wfile.write(bytes(re.findall('"([^"]*)"', response.choices[0].message.content)[0], encoding='utf8'))
 
 class app:
-    def __init__(w, r):
+    def __init__(r):
         print("app")
     
     def run():
