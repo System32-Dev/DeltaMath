5388eccfba46746ef0e995653cb20d59424edfa0
System32-Dev <83769295+System32-Dev@users.noreply.github.com>
2024-04-05 17:55:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,59 +1,36 @@
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from flask import Flask, request, Response
 from g4f.client import Client
+from flask_cors import CORS
 import re
 
 client = Client()
 
-hostName = "0.0.0.0"
-serverPort = 8080
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+app.config['CORS_HEADERS'] = 'X-Unit'
+app.config['CORS_HEADERS'] = 'X-Question'
 
-class MyServer(BaseHTTPRequestHandler):
-    def do_OPTIONS(self):
-        self.send_response(200, "ok")
-        self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Access-Control-Allow-Methods', 'GET, OPTIONS')
-        self.send_header("Access-Control-Allow-Headers", "X-Requested-With")
-        self.send_header("Access-Control-Allow-Headers", "Content-Type")
-        self.send_header("Access-Control-Allow-Headers", "X-Question")
-        self.send_header("Access-Control-Allow-Headers", "X-Unit")
-        self.end_headers()
-        # self.processRequest()
-            
-    def do_GET(self):
-        print("hi")
-        self.send_response(200)
-        self.send_header("Content-type", "text/plain")
-        self.send_header("Access-Control-Allow-Origin", "*")
-        self.end_headers()
-        
-        equation = self.headers.get('X-Question')
-        unit = self.headers.get('X-Unit')
-        
-        response = client.chat.completions.create(
-            model = "gpt-3.5-turbo",
-            messages = [{"role": "user", "content":
-                "I am currentlly working on " + unit + ". Can you please solve " + equation + ". Remember to put the answer in quotation marks."
-            }]
-        )
 
-        self.wfile.write(bytes(re.findall('"([^"]*)"', response.choices[0].message.content)[0], encoding='utf8'))
-
-class app:
-    def __init__(w, x, r):
-        print("app")
+@app.route('/')
+def home():
+    resp = Response("Error", 200)
     
-    def run():
-        webServer = HTTPServer((hostName, serverPort), MyServer)
-        print("Server started http://%s:%s" % (hostName, serverPort))
-
-        try:
-            webServer.serve_forever()
-        except KeyboardInterrupt:
-            pass
-
-        webServer.server_close()
-        print("Server stopped.")
+    equation = request.headers.get('X-Question')
+    unit = request.headers.get('X-Unit')
         
-if __name__ == "__main__":        
+    response = client.chat.completions.create(
+        model = "gpt-3.5-turbo",
+        messages = [{"role": "user", "content":
+            "I am currentlly working on " + unit + ". Can you please solve " + equation + ". Remember to put the answer in quotation marks."
+        }]
+    )
+
+    resp.set_data(re.findall('"([^"]*)"', response.choices[0].message.content)[0])
+    
+    return resp
+
+if __name__ == "__main__":
     app.run()
